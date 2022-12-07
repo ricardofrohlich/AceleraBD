53b0b7942bd61009a74d0ad7bbc4c041928fc5b1
Ricardo Frohlich da Silva <ricardosma@gmail.com>
2022-12-07 10:45:04 -0300

    Add files via upload

---
+++ Contexto.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrimeiraAPI.Models;
+
+namespace PrimeiraAPI.Data
+{
+    public class Contexto : DbContext 
+    {
+        public DbSet<Pessoa> Pessoas { get; set; }
+
+
+        public Contexto()
+        {
+            //Add-Migration Initial
+            //Update-Database
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(@"server=localhost;Port=5432;User ID=postgres;password=admin;database=BD0712");
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
